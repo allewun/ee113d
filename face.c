b4691e565c298a4acf872d5868a311555d4e7bff
Allen Wu <allen.wu@ucla.edu>
2013-12-02 12:45:30 -0800

    Fixed code composer compiler errors

--- face.c
+++ face.c
@@ -3,11 +3,11 @@
 #include <math.h>
 
 
-double  norm(double x[], size_t length);
-double* dot_product(double x[], double y[]);
-double* transpose(double x[], size_t width, size_t height);
+double   norm(double x[], size_t length);
+double*  dot_product(double x[], double y[]);
+double*  transpose(double x[], size_t width, size_t height);
 double** subtract(double **x, double **y, size_t height, size_t width);
-double* max(double x[]);
+double*  max(double x[]);
 
 
 double norm(double x[], size_t length) {
@@ -23,14 +23,15 @@ double norm(double x[], size_t length) {
 double** subtract(double** x, double** y, size_t height, size_t width) {
 
   double** result = malloc(sizeof(double*) * height);
+  int i, h, w;
 
 
-  for (int i = 0; i < height; i++) {
+  for (i = 0; i < height; i++) {
     result[i] = malloc(sizeof(double) * width);
   }
 
-  for (int h = 0; h < height; h++) {
-    for (int w = 0; w < width; w++) {
+  for (h = 0; h < height; h++) {
+    for (w = 0; w < width; w++) {
       result[h][w] = x[h][w] - y[h][w];
     }
   }
@@ -39,19 +40,22 @@ double** subtract(double** x, double** y, size_t height, size_t width) {
 }
 
 double** array2Matrix(double* array, size_t arrayLen, size_t height, size_t width) {
+  double** result;
+  int i, h, w;
+
   if (arrayLen != width * height) {
     return NULL;
   }
 
   // allocate new matrix
-  double** result = malloc(sizeof(double*) * height);
-  for (int i = 0; i < height; i++) {
+  result = malloc(sizeof(double*) * height);
+  for (i = 0; i < height; i++) {
     result[i] = malloc(sizeof(double) * width);
   }
 
   // fill it in
-  for (int h = 0; h < height; h++) {
-    for (int w = 0; w < width; w++) {
+  for (h = 0; h < height; h++) {
+    for (w = 0; w < width; w++) {
       result[h][w] = array[h*width + w];
     }
   }
@@ -59,11 +63,8 @@ double** array2Matrix(double* array, size_t arrayLen, size_t height, size_t widt
   return result;
 }
 
-// double* transpose(double x[], size_t width, size_t height) {
 
-// }
-
-int main() {
+void main() {
 
 
   double test1[2][5] = {{1.0, 2.0, 3.0, 4.0, 5.0},
@@ -72,32 +73,26 @@ int main() {
   double test2[2][5] = {{4.0, 2.0, 7.0, 1.0, 2.0},
                         {1.6, 2.0, 3.0, 4.0, 5.6}};
 
-  double** matrix1 = array2Matrix(test1, 10, 2, 5);
-  double** matrix2 = array2Matrix(test2, 10, 2, 5);
+  double** matrix1 = array2Matrix((double*)test1, 10, 2, 5);
+  double** matrix2 = array2Matrix((double*)test2, 10, 2, 5);
+
+  double** subtracted;
+
+  int i, h, w;
+
+
 
   // Norm
-  printf("Norm: %f\n\n", norm(test1, 5));
+  printf("Norm: %f\n\n", norm((double*)test1, 5));
 
 
   // Subtract
   printf("Subtract: ");
-  double** subtracted = subtract(matrix1, matrix2, 2, 5);
-  for (int h = 0; h < 2; h++) {
-    for (int w = 0; w < 5; w++) {
+  subtracted = subtract(matrix1, matrix2, 2, 5);
+  for (h = 0; h < 2; h++) {
+    for (w = 0; w < 5; w++) {
       printf("%f ", subtracted[h][w]);
     }
   }
   printf("\n\n");
-
-
- // return 0;
-
 }
-
-
-
-// [1, 2, 3, 4,5 ,6]
-
-// p = [a = [2,3,4],[4,5,7]]
-
-// p -> a -> 2
