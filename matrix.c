020bd75f45585b0625d25cd372c8a74ab6092984
Allen Wu <allen.wu@originate.com>
2013-12-05 22:05:17 -0800

    cleanup

--- matrix.c
+++ matrix.c
@@ -20,7 +20,7 @@
 // Convert a plain array to a Matrix struct
 Matrix array2Matrix(double* array, size_t arrayLen, size_t rows, size_t cols) {
     Matrix result = {0};
-    int i, h, w;
+    int r, c;
 
     // verify dimensions
     if (arrayLen != cols * rows) {
@@ -31,9 +31,9 @@ Matrix array2Matrix(double* array, size_t arrayLen, size_t rows, size_t cols) {
     result = newMatrix(rows, cols, false);
 
     // populate data
-    for (h = 0; h < rows; h++) {
-        for (w = 0; w < cols; w++) {
-            result.data[h][w] = array[h*cols + w];
+    for (r = 0; r < rows; r++) {
+        for (c = 0; c < cols; c++) {
+            result.data[r][c] = array[r*cols + c];
         }
     }
 
@@ -41,25 +41,25 @@ Matrix array2Matrix(double* array, size_t arrayLen, size_t rows, size_t cols) {
 }
 
 // Print matrix to console
-void printMatrix(Matrix m) {
-    int i, j;
+void printMatrix(Matrix matrix) {
+    int r, c;
 
     printf("\n");
-    if (m.cols == 0 || m.rows == 0) {
+    if (matrix.cols == 0 || matrix.rows == 0) {
         printf("[]\n");
         return;
     }
-    for (i = 0; i < m.rows; i++) {
-        for (j = 0; j < m.cols; j++) {
-            if (i == 0 && j == 0) {
+    for (r = 0; r < matrix.rows; r++) {
+        for (c = 0; c < matrix.cols; c++) {
+            if (r == 0 && c == 0) {
                 printf("[ ");
             }
-            else if (j == 0) {
+            else if (c == 0) {
                 printf("  ");
             }
-            printf("%.*f ", PRINT_MATRIX_DECIMALS, m.data[i][j]);
+            printf("%.*f ", PRINT_MATRIX_DECIMALS, matrix.data[r][c]);
         }
-        if (i == m.rows - 1) {
+        if (r == matrix.rows - 1) {
             printf("]");
         }
         printf("\n");
@@ -67,16 +67,16 @@ void printMatrix(Matrix m) {
 }
 
 // Free memory allocated for matrix data
-void freeMatrix(Matrix* m) {
+void freeMatrix(Matrix* matrix) {
     int i;
-    for (i = 0; i < m->rows; i++) {
-        free(m->data[i]);
+    for (i = 0; i < matrix->rows; i++) {
+        free(matrix->data[i]);
     }
-    free(m->data);
+    free(matrix->data);
 
-    m->cols = 0;
-    m->rows = 0;
-    m->data = NULL;
+    matrix->cols = 0;
+    matrix->rows = 0;
+    matrix->data = NULL;
 }
 
 // Allocate an rxc block of memory to be used for a matrix
@@ -104,8 +104,8 @@ Matrix newMatrix(size_t rows, size_t cols, bool clearMemory) {
 
 // Calculate vector norm
 double vectorNorm(Matrix a) {
-    double sum = 0;
-    int i, j;
+    double sum = 0.0;
+    int i;
 
     // calculate vector norm if matrix is a vector
     if (a.cols == 1) {
@@ -128,19 +128,19 @@ double vectorNorm(Matrix a) {
 }
 
 // Find max element of vector
-Pair maxWithIndex(Matrix a) {
+Pair maxWithIndex(Matrix matrix) {
     double currentMax = -DBL_MAX;
     int currentIndex = -1;
     Pair result = {-1, -1};
 
     int r, c, rMax, cMax;
 
-    if (a.rows == 1) {
+    if (matrix.rows == 1) {
         rMax = 1;
-        cMax = a.cols;
+        cMax = matrix.cols;
     }
-    else if (a.cols == 1) {
-        rMax = a.rows;
+    else if (matrix.cols == 1) {
+        rMax = matrix.rows;
         cMax = 1;
     }
     else {
@@ -149,8 +149,8 @@ Pair maxWithIndex(Matrix a) {
 
     for (r = 0; r < rMax; r++) {
         for (c = 0; c < cMax; c++) {
-            if (a.data[r][c] > currentMax) {
-                currentMax = a.data[r][c];
+            if (matrix.data[r][c] > currentMax) {
+                currentMax   = matrix.data[r][c];
                 currentIndex = r+c;
             }
         }
@@ -164,7 +164,7 @@ Pair maxWithIndex(Matrix a) {
 
 // Sum of all elements of matrix
 double sum(Matrix a) {
-    double result = 0;
+    double result = 0.0;
     int r, c;
 
     for (r = 0; r < a.rows; r++) {
@@ -178,7 +178,7 @@ double sum(Matrix a) {
 // Perform matrix subtraction
 Matrix subtract(Matrix a, Matrix b) {
     Matrix result = {0};
-    int i, h, w;
+    int r, c;
 
     // verify dimensions
     if (a.cols != b.cols || a.rows != b.rows) {
@@ -189,9 +189,9 @@ Matrix subtract(Matrix a, Matrix b) {
     result = newMatrix(a.rows, a.cols, false);
 
     // subtract
-    for (h = 0; h < a.rows; h++) {
-        for (w = 0; w < a.cols; w++) {
-            result.data[h][w] = a.data[h][w] - b.data[h][w];
+    for (r = 0; r < a.rows; r++) {
+        for (c = 0; c < a.cols; c++) {
+            result.data[r][c] = a.data[r][c] - b.data[r][c];
         }
     }
 
@@ -201,7 +201,7 @@ Matrix subtract(Matrix a, Matrix b) {
 // Perform matrix dot product
 Matrix dotProduct(Matrix a, Matrix b) {
     Matrix result = {0};
-    int i, h, w, yh;
+    int h, w, yh;
 
     // verify dimensions
     if (a.cols != b.rows) {
@@ -226,15 +226,15 @@ Matrix dotProduct(Matrix a, Matrix b) {
 // Transpose matrix
 Matrix transpose(Matrix a) {
     Matrix result = {0};
-    int i, j;
+    int r, c;
 
     // create matrix
     result = newMatrix(a.cols, a.rows, false);
 
     // swap a[r][c] with a[c][r]
-    for (i = 0; i < a.rows; i++) {
-        for (j = 0; j < a.cols; j++) {
-            result.data[j][i] = a.data[i][j];
+    for (r = 0; r < a.rows; r++) {
+        for (c = 0; c < a.cols; c++) {
+            result.data[c][r] = a.data[r][c];
         }
     }
 
@@ -244,7 +244,7 @@ Matrix transpose(Matrix a) {
 // Extract the nth column of a matrix
 Matrix column(Matrix a, int n) {
     Matrix result = {0};
-    int i, h;
+    int r;
 
     // verify dimensions
     if (n > a.cols-1 || n < 0) {
@@ -255,8 +255,8 @@ Matrix column(Matrix a, int n) {
     result = newMatrix(a.rows, 1, false);
 
     // populate data
-    for (h = 0; h < a.rows; h++) {
-        result.data[h][0] = a.data[h][n];
+    for (r = 0; r < a.rows; r++) {
+        result.data[r][0] = a.data[r][n];
     }
 
     return result;
--- test.h
+++ test.h
@@ -14,6 +14,7 @@
 #include <assert.h>
 
 void testMatrix();
+void testFace();
 
 
 #endif
