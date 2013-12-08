006834253994bc89f8354a1967f664c3ea122cd3
Allen Wu <allen.wu@ucla.edu>
2013-12-08 06:43:13 -0800

    optimized order for memory

--- face.c
+++ face.c
@@ -49,21 +49,10 @@ void genderDetection(char* file) {
     float* bmpData;
     Pair maxIndex;
 
-    features = array2Matrix((float*)featuresArray, 20*32, 20, 32);
-
     // load input_image
     bmpData = loadBitmapFileGrayscale(file, &bmpInfoHeader);
     input_image = array2Matrix((float*)bmpData, 180*200, 200, 180);
     free(bmpData);
-    //printMatrix(input_image);
-
-    // load mean_face
-    bmpData = loadGrayBitmapFileGrayscale(MEANFACE_FILE, &bmpInfoHeader);
-    mean_face = array2Matrix((float*)bmpData, 180*200, 36000, 1);
-    free(bmpData);
-
-    // load evectors
-    evectors = array2Matrix((float*)evecs, 36000*20, 20, 36000);
 
     // transpose input_image
     transposed_input_image = transpose(input_image);
@@ -73,10 +62,23 @@ void genderDetection(char* file) {
     transposed_reshaped_input_image = reshape(transposed_input_image, 36000, 1);
     freeMatrix(&transposed_input_image);
 
+    // load evectors
+    evectors = array2Matrix((float*)evecs, 36000*20, 20, 36000);
+
+    // load mean_face
+    bmpData = loadGrayBitmapFileGrayscale(MEANFACE_FILE, &bmpInfoHeader);
+    mean_face = array2Matrix((float*)bmpData, 180*200, 36000, 1);
+    free(bmpData);
+
     faceMinusMean = subtract(transposed_reshaped_input_image, mean_face);
+    freeMatrix(&transposed_reshaped_input_image);
+    freeMatrix(&mean_face);
+
     featureVec = dotProduct(evectors, faceMinusMean);
+    freeMatrix(&faceMinusMean);
 
     // similarity score
+    features = array2Matrix((float*)featuresArray, 20*32, 20, 32);
     similarity_score = similarityScore(features, featureVec, 32);
     printMatrix(similarity_score);
 
