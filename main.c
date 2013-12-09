d5c69389202243ab90c4a5e901f549df8af28cd7
Allen Wu <allen.wu@ucla.edu>
2013-12-08 18:44:38 -0800

    cleanup

--- main.c
+++ main.c
@@ -8,18 +8,21 @@
 //  * Bryant Khau
 //
 
-#include "test.h"
 #include "bmp.h"
 #include "face.h"
 
 void main() {
-    char* people[8] = {"briggs.bmp", "bryant.bmp", "allen.bmp", "cabric.bmp", "bryant2.bmp", "abidi.bmp", "feminine.bmp", "hayden.bmp"};
+    char* people[] = {"briggs.bmp",
+                      "bryant.bmp",
+                      "allen.bmp",
+                      "cabric.bmp",
+                      "bryant2.bmp",
+                      "abidi.bmp",
+                      "feminine.bmp",
+                      "hayden.bmp"};
     int i;
 
-    testMatrix();
-    testFace();
-
-    for (i = 0; i < 8; i++) {
+    for (i = 0; i < sizeof(people)/sizeof(people[0]); i++) {
       genderDetection(people[i]);
     }
 }
